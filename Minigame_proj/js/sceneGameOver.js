9c5b3e38dac55b73660e2b7053edb319f354df25
Guilherme Baroni Marchiori <guibaroni4466700@gmail.com>
2020-11-24 20:51:52 -0300

    sons e controle de sons adicionados, tem algum problema com a musica, nao tenho ideia doq(se pa eh a referencia do arquivo)

--- Minigame_proj/js/sceneGameOver.js
+++ Minigame_proj/js/sceneGameOver.js
@@ -26,6 +26,6 @@ class SceneGameOver extends Phaser.Scene {
     }
     update() 
     {
-
+        
     }
 }
--- Minigame_proj/js/sceneMain.js
+++ Minigame_proj/js/sceneMain.js
@@ -9,19 +9,28 @@ class SceneMain extends Phaser.Scene
         // carrega as imagens
         this.load.image('plane', 'images/plane_1_red.png');
         this.load.image('curse', 'images/hazard_base.png');
+        //carrega os audios
         this.load.audio("planeSound1",["sons/loop_aviao_alterado(continuo).mp3"], "sons/loop_aviao_alterado(continuo).ogg");
         this.load.audio("planeSound2",["sons/loop_aviao_alterado(parte_interior).mp3"], "sons/loop_aviao_alterado(parte_interior).ogg");
         this.load.audio("planeJumpSound", ["sons/404793__owlstorm__retro-video-game-sfx-jump.wav"]);
+        this.load.audio("music1Action", ["ActionMusicEditada.wav"]);
     }
     create() 
     {
-        //sons do avião
+        //Sons do avião
         this.planeSound1 = this.sound.add('planeSound1', {volume: 0.3});
         this.planeSound2 = this.sound.add('planeSound2', {volume: 0.3});
+        this.planeJumpSound = this.sound.add('planeJumpSound', {volume: 0.3});
+        //Musica
+        this.music1Action = this.sound.add('music1Action', {volume: 0.3});
+        //Play the sounds and loop those that should loop
         this.planeSound1.play();
         this.planeSound2.play();
+        this.music1Action.play();
+
         this.planeSound1.loop = true;
         this.planeSound2.loop = true;
+        this.music1Action.loop = true;
 
         // adiciona o score no jogo
         this.score = -1;
@@ -49,6 +58,9 @@ class SceneMain extends Phaser.Scene
     }
     movePlane() 
     {
+        //Som do aviao pulando
+        this.planeJumpSound.play();
+
         // define a velocidade da gravidade do avião a cada clique/tecla
         this.plane.setVelocity(0, -300);
     }
@@ -77,6 +89,11 @@ class SceneMain extends Phaser.Scene
     }
     endGame() 
     {
+        //Para todos os sons em loop (do aviao e da musica)
+        this.planeSound1.stop();
+        this.planeSound2.stop();
+        this.music1Action.stop();
+
         // reinicia o jogo;
         this.scene.start('SceneGameOver');
     }
